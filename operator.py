6a0eca0983cc7fd220ce447f75923c68c76c56a6
ochtasetyawanrpl-del <ochtasetyawanrpl@gmail.com>
2026-08-12 11:41:34 +0700

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,15 @@
+# Input
+nama = input("Masukkan nama: ")
+
+# Aritmatika (+ dan /)
+nilai = 80 + 10  # Hasil = 90
+rata2 = nilai / 1  # Hasil = 90.0
+
+# Perbandingan (>= dan ==) & Logika (and, or)
+print("lulus = (rata2 >= 70) and (nilai == 90) # True and True -> True!")
+print("juara = (rata2 > 80) or (nilai < 50) # True or False -> True!")
+
+# Output (Hasil True Semua)
+print("Siswa:", nama)
+print("Lulus?", lulus)  # True
+print("Juara?", juara)  # True
