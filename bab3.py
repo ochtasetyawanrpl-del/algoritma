04ab0c717bb98782fa800d59a6053567e6a8b01f
ochtasetyawanrpl-del <ochtasetyawanrpl@gmail.com>
2026-08-12 07:48:24 +0700

    Add files via upload

---
+++ bab3.py
@@ -0,0 +1,17 @@
+nama="ochta"
+umur=16
+tinggi="5 meter"
+lulus=True
+print("nama : ",nama )
+print("umur : ",umur)
+print("tinggi : ",tinggi)
+print("lulus : ", lulus)
+print()
+produk="sepatu lari merk Adidas"
+stok=100
+harga="5jt"
+Tersedia="True"
+print("nama produk : ",produk)
+print("Stok : ",stok)
+print("harga :",harga)
+print("Tersedia :", Tersedia)
